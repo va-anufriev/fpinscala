6557b0b6b7deb36c2932f74920274ea7ae5e60e0
Vadim Anufriev <canny_wasp@vaan.io>
2020-10-02 15:36:33 +0300

    chapter 3 => tree - size

---
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -0,0 +1,20 @@
+package fpinscala.datastructures
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class TreeSpec extends AnyFlatSpec {
+  val tree: Branch[Int] = Branch(
+    left = Branch(
+      left = Leaf(1),
+      right = Leaf(2)
+    ),
+    right = Branch(
+      left = Leaf(3),
+      right = Leaf(4)
+    )
+  )
+
+  it should "count size" in {
+    assert(Tree.size(tree) == 7)
+  }
+}
