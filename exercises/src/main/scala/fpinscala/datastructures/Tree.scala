2433d58ac1a9c7d07b74ace73aee7d2ceff61828
Vadim Anufriev <canny_wasp@vaan.io>
2020-10-05 10:06:41 +0300

    chapter 3 => tree - fold

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -1,6 +1,22 @@
 package fpinscala.datastructures
 
-sealed trait Tree[+A]
+sealed trait Tree[+A] {
+  def map[B](f: A => B): Tree[B] =
+    this match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(left, right) => Branch(left.map(f), right.map(f))
+    }
+
+  def fold[B](f: A => B)(g: (B, B) => B): B =
+    this match {
+      case Leaf(value) => f(value)
+      case Branch(left, right) => g(left.fold(f)(g), right.fold(f)(g))
+    }
+
+  def size: Int =
+    fold[Int](_ => 1)((x, y) => 1 + x + y)
+}
+
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
@@ -16,5 +32,23 @@ object Tree {
     tree match {
       case Leaf(value) => value
       case Branch(left, right) => maximum(left) max maximum(right)
+    }
+
+  def maximum[A](tree: Tree[A])(max: (A, A) => A): A =
+    tree match {
+      case Leaf(value) => value
+      case Branch(left, right) => max(maximum(left)(max), maximum(right)(max))
+    }
+
+  def depth[A](tree: Tree[A]): Int =
+    tree match {
+      case Leaf(_) => 1
+      case Branch(left, right) => 1 + depth(left) max 1 + depth(right)
+    }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+    tree match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
     }
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -18,7 +18,47 @@ class TreeSpec extends AnyFlatSpec {
     assert(Tree.size(tree) == 7)
   }
 
+  it should "count size - via fold" in {
+    assert(tree.size == 7)
+  }
+
   it should "find max value in Tree[Int]" in {
     assert(Tree.maximum(tree) == 4)
+  }
+
+  it should "find max value in Tree[A]" in {
+    Tree.maximum[Int](tree)(_ max _)
+  }
+
+  it should "count depth" in {
+    val tree =
+      Branch(
+        left = Leaf(1),
+        right = Branch(
+          left = Branch(
+            left = Leaf(3),
+            right = Leaf(4)
+          ),
+          right = Leaf(5)
+        )
+      )
+
+    assert(Tree.depth(tree) == 4)
+  }
+
+  it should "map" in {
+    val expected: Branch[Int] = Branch(
+      left = Branch(
+        left = Leaf(1),
+        right = Leaf(4)
+      ),
+      right = Branch(
+        left = Leaf(9),
+        right = Leaf(16)
+      )
+    )
+
+    assert(Tree.map(tree)(x => x * x) == expected)
+    assert(tree.map(x => x * x) == expected)
   }
 }
