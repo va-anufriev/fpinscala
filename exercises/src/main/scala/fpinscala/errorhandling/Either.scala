2a5e9db4c6608b65432325fd29ef8b3751059ea1
Vadim Anufriev <vadim.anufriev@x5.ru>
2020-10-10 23:11:28 +0300

    chapter 4 => ex 4.6, either - map, flatMap, orElse, map2

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,13 +4,26 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B] =
+   this match {
+     case Right(value) => Right(f(value))
+     case Left(value) => Left(value)
+   }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+   this match {
+     case Right(get) => f(get)
+     case Left(get) => Left(get)
+   }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+   this match {
+     case Right(get) => Right(get)
+     case Left(_) => b
+   }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    flatMap (aa => b map (bb => f(aa, bb)))
 }
 
 case class Left[+E](get: E) extends Either[E,Nothing]
--- exercises/src/test/scala/fpinscala/errorhandling/EitherSpec.scala
+++ exercises/src/test/scala/fpinscala/errorhandling/EitherSpec.scala
@@ -4,10 +4,45 @@ import org.scalatest.flatspec.AnyFlatSpec
 import scala.{Option => _, Either => _, Left => _, Right => _, _}
 
 class EitherSpec extends AnyFlatSpec {
+  val left: Either[Exception, Int] = Left(new Exception)
+
   it should "mean" in {
     assert(Either.mean(IndexedSeq[Double](1, 2, 3)) match {
       case Right(_) => true
       case Left(_) => false
     })
+  }
+
+  it should "map for Right" in {
+    assert(Right(4).map(x => x * x) == Right(16))
+  }
+
+  it should "map for Left" in {
+    assert(left.map(x => x * x) == left)
+  }
+
+  it should "orElse for Right" in {
+    assert(Right(4).orElse(Right(0)) == Right(4))
+  }
+
+  it should "orElse for Left" in {
+    assert(left.orElse(Right(0)) == Right(0))
+  }
+
+  it should "flatMap Right with f => Right" in {
+    assert(Right(4).flatMap(x => Right(x * x)) == Right(16))
+  }
+
+  it should "flatMap Right with f => Left" in {
+    assert(Right(4).flatMap(_ => left) == left)
+  }
+
+  it should "flatMap Left" in {
+    assert(left.flatMap(_ => Right(0)) == left)
+  }
+
+  it should "map2 - Right and Right is Right" in {
+    val result = Right(4).map2[Int, Double, Double](Right(3.0))((a, b) => a * b)
+    assert(result == Right(12.0))
   }
 }
